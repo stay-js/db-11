8646311084ca42b999aa1b6aa03becfad6cca0ed
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-09 12:32:30 +0100

    add jatekbolt and hr feladat

--- 25-jatekbolt-ek-feladat/asd.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `jatek` (`cikkszam`, `ar`, `korhatar_min`, `korhatar_max`, `jatekosok_szama_min`, `jatekosok_szama_max`, `nyelv`) VALUES
-('HPHOGW', 'Harry Potter Hogwarts Battle Monster box angol nyelvű kiegészítő', 10490, 11, NULL, 2, 4,'magyar')
-('DB105', 'Harry Potter: Roxforti csata társasjáték', 19990, 11, NULL, 2, 4, 'magyar')
-('GDR44', 'Uno Flip kártyajáték', 2790, 7, NULL, 2, 10, 'magyar')
-('GWDES', 'A Tiltott Sivatag társasjáték', 8490, 10, NULL, 2, 5 ,'magyar')
-('WM00396NULLML1NULL6', 'Rick and Morty 1000 db puzzle', 5490, 3, 100, 1, 6, 'francia');
