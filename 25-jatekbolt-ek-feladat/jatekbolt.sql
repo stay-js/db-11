79ec708e8fb4b2c33eb571f0d682601e3d064a00
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-10 14:21:46 +0100

    update jatekbolt feladat

--- 25-jatekbolt-ek-feladat/jatekbolt.sql
+++ 25-jatekbolt-ek-feladat/jatekbolt.sql
@@ -22,7 +22,8 @@ CREATE TABLE `jatek` (
 DROP TABLE IF EXISTS `gyarto`;
 
 CREATE TABLE `gyarto` (
-	`weboldal` varchar(250) PRIMARY KEY NOT NULL,
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`weboldal` varchar(250) NOT NULL,
 	`nev` varchar(50) NOT NULL
 );
 
@@ -30,10 +31,10 @@ DROP TABLE IF EXISTS `gyartja`;
 
 CREATE TABLE `gyartja` (
 	`cikkszam` varchar(20) NOT NULL,
-	`weboldal` varchar(250) NOT NULL,
-	PRIMARY KEY (cikkszam, weboldal),
-	FOREIGN KEY (cikkszam) REFERENCES jatek (cikkszam),
-	FOREIGN KEY (weboldal) REFERENCES gyarto (weboldal)
+	`gyarto_id` int NOT NULL,
+	PRIMARY KEY (cikkszam, gyarto_id)
+	-- FOREIGN KEY (cikkszam) REFERENCES jatek (cikkszam),
+	-- FOREIGN KEY (gyarto_id) REFERENCES gyarto (id)
 );
 
 DROP TABLE IF EXISTS `kategoria`;
@@ -46,11 +47,11 @@ CREATE TABLE `kategoria` (
 DROP TABLE IF EXISTS `kateg`;
 
 CREATE TABLE `kateg` (
-	`kateg_id` int NOT NULL,
 	`cikkszam` varchar(20) NOT NULL,
-	PRIMARY KEY (kateg_id, cikkszam),
-	FOREIGN KEY (kateg_id) REFERENCES kategoria (id),
-	FOREIGN KEY (cikkszam) REFERENCES jatek (cikkszam)
+	`kateg_id` int NOT NULL,
+	PRIMARY KEY (cikkszam, kateg_id)
+	-- FOREIGN KEY (cikkszam) REFERENCES jatek (cikkszam),
+	-- FOREIGN KEY (kateg_id) REFERENCES kategoria (id)
 );
 
 INSERT INTO `jatek` (`cikkszam`, `cim`, `ar`, `korhatar_min`, `korhatar_max`, `jatekosok_szama_min`, `jatekosok_szama_max`, `nyelv`) VALUES
@@ -60,18 +61,18 @@ INSERT INTO `jatek` (`cikkszam`, `cim`, `ar`, `korhatar_min`, `korhatar_max`, `j
 ('GWDES', 'A Tiltott Sivatag társasjáték', 8490, 10, NULL, 2, 5 ,'magyar'),
 ('WM00396NULLML1NULL6', 'Rick and Morty 1000 db puzzle', 5490, 3, 100, 1, 6, 'francia');
 
-INSERT INTO `gyarto` (`weboldal`, `nev`) VALUES
+INSERT INTO `gyarto` (`nev`, `weboldal`) VALUES
 ('The Op', 'https://theop.games'),
 ('Mattel Inc.', 'https://mattel.com'),
 ('Gamewright', 'https://gamewright.com'),
 ('Winning Moves Games',	'https://www.winning-moves.com');
 
-INSERT INTO `gyartja` (`cikkszam`, `weboldal`) VALUES
-('HPHOGW', 'https://theop.games'),
-('DB105', 'https://theop.games'),
-('GDR44', 'https://mattel.com'),
-('GWDES', 'https://gamewright.com'),
-('WM00396-ML1-6', 'https://www.winning-moves.com');
+INSERT INTO `gyartja` (`cikkszam`, `gyarto_id`) VALUES
+('HPHOGW', 1),
+('DB105', 1),
+('GDR44', 2),
+('GWDES', 3),
+('WM00396-ML1-6', 4);
 
 INSERT INTO `kategoria` (`nev`) VALUES
 ('deck-buildig'),
@@ -81,3 +82,17 @@ INSERT INTO `kategoria` (`nev`) VALUES
 ('kompetitív'),
 ('táblajáték'),
 ('puzzle');
+
+INSERT INTO `kateg` (`cikkszam`, `kateg_id`) VALUES
+('HPHOGW', 1),
+('HPHOGW', 2),
+('HPHOGW', 3),
+('DB105', 1),
+('DB105', 2),
+('DB105', 3),
+('DB105', 4),
+('GDR44', 3),
+('GDR44', 5),
+('GWDES', 2),
+('GWDES', 6),
+('WM00396-ML1-6', 7);
